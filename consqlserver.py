2342c860496bdd4328dc2d0f6b37aee1a2930d0b
Cristian Lancheros <clancheros@barranquilla.cdstecnosas>
2021-06-22 15:46:49 -0500

    subida archivo de config

---
+++ consqlserver.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+server = 'tcp:192.168.1.127,10433'
+db = 'TecnoCarnes'
+user = 'sa'
+password = '3.1416.asd*'
+
+try:
+    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+password)
+
+    print("\n")
+    print("Conexion exitosa !")
+    print("\n")
+
+except Exception as e:
+    print("Ocurrio un error al conectar SQL Server: ", e)
